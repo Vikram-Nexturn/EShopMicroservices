81f81aa5190c0673d80bff39b869e1f49f5d14a8
Vikram-Nexturn <Vikram.kr@nexturn.com>
2025-09-13 22:42:43 +0530

    Till Section 7

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,53 @@
+﻿
+using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if(errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
+            // create a product entiry.
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // save to db
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+
+            // return
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.GetProduct;
+
+namespace Catalog.API.Products.GetProductById
+{
+    //public record GetProductByIdRequest();
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.Map("/products/{id}", async (Guid id, ISender sender) => 
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product by id")
+            .WithDescription("Get Product by id");
+        }
+    }
+}
